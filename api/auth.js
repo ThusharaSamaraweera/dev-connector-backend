73b3f6eefd69e6eee4aea7e06fa9797824796811
Thushara Samaraweera <thusharadasun204@gmail.com>
2021-12-19 04:58:28 +0530

    check authentication

--- api/auth.js
+++ api/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 
 // @router  GET API/auth
 // @desc    Test router
 // @access  Public
 
-router.get('/', (req, res) => res.send('Auth router'));
+router.get('/', auth, (req, res) => res.send('Auth router'));
 
 module.exports = router;
