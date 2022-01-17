c1d211263e61309a041486774cddb42a29d3050f
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-01-17 10:59:36 +0530

    add functionality of adding posts

--- route/api/posts.js
+++ route/api/posts.js
@@ -1,10 +1,44 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../../middleware/auth');
+
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
 const router = express.Router();
 
-// @router  GET API/posts
-// @desc    Test router
-// @access  Public
+// @router  POST api/posts
+// @desc    Create post
+// @access  private
 
-router.get('/', (req, res) => res.send('Posts router'));
+router.post('/', [
+  auth,
+  [
+    check('text', 'Text is required').not().isEmpty(),
+  ]
+], async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(400).json({error: errors.array() });
+  }
+
+  try {
+    const user =  await User.findById(req.user.id).select('-password');
+    const newPost = new Post({
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    })
+
+    const post = await newPost.save();
+    res.json(post);
+
+  } catch (err) {
+    console.error(err.mes);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
